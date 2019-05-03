240d436aa9a5a731f795d06a162529ffa1dfbb10
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-05-03 10:34:38 +0200

    SVM-311 - alt-Attribut durch title-Attribut in Links ersetzen

--- lib/frontend/tpl/archive/themes/grid.php
+++ lib/frontend/tpl/archive/themes/grid.php
@@ -21,7 +21,7 @@
 								
 								if ( ! empty( $categories ) ) {
 									foreach ( $categories as $category ) {
-										$output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="'
+										$output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="'
 												   . esc_attr( sprintf( __( 'View all posts in %s', $this->get_module_name() ), $category->name ) ) .
 												   '" class="' . $this->get_prefix( 'category' ) .'">'
 												   . esc_html( $category->name ) . '</a>' . $separator;
