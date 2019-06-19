86a6f9f62a012b221e892c765978ee141b7d07f6
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-19 12:54:30 +0200

    Added author name to single posts

--- lib/frontend/tpl/content/single_sidebar.php
+++ lib/frontend/tpl/content/single_sidebar.php
@@ -1,3 +1,7 @@
+<?php
+	while ( have_posts() ) {
+	the_post();
+?>
 <div class="<?php echo $this->get_prefix() . ' ' . $this->get_prefix( 'single' ); ?>">
 	<?php
 		$class = $this->get_prefix( 'header' );
@@ -12,7 +16,10 @@
 		<div class="<?php echo $this->get_prefix( 'header_content' ); ?>">
 			<h1><?php the_title()?></h1>
 			<div class="<?php echo $this->get_prefix( 'excerpt' ); ?>"><?php the_excerpt(); ?></div>
-			<span><?php echo get_the_date(); ?></span>
+			<div>
+				<span><?php echo get_the_author(); ?></span>
+				<span><?php echo get_the_date(); ?></span>
+			</div>
 		</div>
 		<div class="<?php echo $this->get_prefix( 'header_background' ); ?>">
 			<?php
@@ -27,35 +34,28 @@
 		</div>
 	</div>
 	<div class="<?php echo $this->get_prefix( 'wrapper' ) . ' ' . $this->get_prefix( 'with_sidebar' ); ?>">
-		<?php
-			while ( have_posts() ) {
-				the_post();
-				?>
-				<article id="post-<?php echo the_ID(); ?>" <?php post_class(); ?>>
-					<?php
-						the_content();
-						wp_link_pages(
-							array(
-								'before'      => '<div class="' . $this->get_prefix( 'page_links' ) . '">',
-								'after'       => '</div>',
-								'link_before' => '',
-								'link_after'  => '',
-								'pagelink'    => __( 'Page', 'straightvisions_100' ) . ' %',
-								'separator'   => '',
-							)
-						);
-						
-						echo $this->get_root()->get_module( 'sv_tags' )
-							? $this->get_root()->get_module( 'sv_tags' )->load()
-							: '';
-						echo $this->get_root()->get_module( 'sv_comments' )
-							? $this->get_root()->get_module( 'sv_comments' )->load()
-							: '';
-					?>
-				</article>
-				<?php
-			}
-		?>
+		<article id="post-<?php echo the_ID(); ?>" <?php post_class(); ?>>
+			<?php
+				the_content();
+				wp_link_pages(
+					array(
+						'before'      => '<div class="' . $this->get_prefix( 'page_links' ) . '">',
+						'after'       => '</div>',
+						'link_before' => '',
+						'link_after'  => '',
+						'pagelink'    => __( 'Page', 'straightvisions_100' ) . ' %',
+						'separator'   => '',
+					)
+				);
+				
+				echo $this->get_root()->get_module( 'sv_tags' )
+					? $this->get_root()->get_module( 'sv_tags' )->load()
+					: '';
+				echo $this->get_root()->get_module( 'sv_comments' )
+					? $this->get_root()->get_module( 'sv_comments' )->load()
+					: '';
+			?>
+		</article>
 		<aside class="<?php echo $this->get_prefix( 'sidebar' ); ?>">
 			<?php
 				echo $this->get_root()->get_module( 'sv_sidebar' )
@@ -67,3 +67,6 @@
 		</aside>
 	</div>
 </div>
+<?php
+	}
+?>
