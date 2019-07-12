f0ba468d88036e6c5f8819ab86766843acafb419
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-12 11:04:32 +0200

    SVM-278 - Kategorie Description in Kategorie Auflistung

--- lib/frontend/tpl/archive/category.php
+++ lib/frontend/tpl/archive/category.php
@@ -1,6 +1,7 @@
 <div class="<?php echo $this->get_prefix() . ' ' .$this->get_prefix( 'category' ); ?>">
 	<div class="<?php echo $this->get_prefix( 'header' ); ?>">
 		<h1><?php _e( 'Category', 'sv100' ); ?>: <span><?php single_cat_title(); ?></span></h1>
+		<?php echo category_description(); ?>
 	</div>
 	
 	<?php if ( have_posts() ) { ?>
