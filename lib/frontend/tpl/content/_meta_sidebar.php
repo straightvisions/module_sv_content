d2b41ec1bc94a7bbb1cd092584c96f923b6f34e4
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-08-07 12:21:02 +0200

    Renamed var due to Theme Sniffer Error

--- lib/frontend/tpl/content/_meta_sidebar.php
+++ lib/frontend/tpl/content/_meta_sidebar.php
@@ -1,5 +1,5 @@
 <?php
-	$type = is_single() ? 'post' : 'page';
+	$type_post = is_single() ? 'post' : 'page';
 	
 	if ( $this->show_right_sidebar() ) {
 		$this->get_script( 'widgets' )->set_inline( true );
@@ -7,26 +7,26 @@
 	
 	$this->get_script( 'widgets' )->set_is_enqueued();
 	
-	if ( $this->show_right_sidebar() && ! empty( $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type . '_right' ) ) ) ) {
+	if ( $this->show_right_sidebar() && ! empty( $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type_post . '_right' ) ) ) ) {
 		$this->has_sidebar = true;
 		$this->get_script( 'sidebar_right' )->set_is_enqueued();
 	?>
 	<aside class="<?php echo $this->get_prefix( 'sidebar_right' ); ?>">
 		<?php
-			echo $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type . '_right' ) );
+			echo $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type_post . '_right' ) );
 			
-			if ( $this->get_setting( 'sidebar_right_' . $type . '_sticky' )->run_type()->get_data() ) {
+			if ( $this->get_setting( 'sidebar_right_' . $type_post . '_sticky' )->run_type()->get_data() ) {
 				$this->get_script( 'sidebar_js' )->set_is_enqueued();
 			}
 		?>
 	</aside>
-<?php } if ( $this->show_bottom_sidebar() && ! empty( $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type . '_bottom' ) ) ) ) {
+<?php } if ( $this->show_bottom_sidebar() && ! empty( $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type_post . '_bottom' ) ) ) ) {
 		$this->has_sidebar = true;
 		$this->get_script( 'sidebar_bottom' )->set_is_enqueued();
 	?>
 	<aside class="<?php echo $this->get_prefix( 'sidebar_bottom' ); ?>">
 		<?php
-			echo $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type . '_bottom' ) );
+			echo $this->get_module( 'sv_sidebar' )->load( array( 'id' => $this->get_module_name() . '_' . $type_post . '_bottom' ) );
 		?>
 	</aside>
 <?php } ?>
