66d35692e4a45db0229a7e162ff7515df4c1bf87
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-08-14 12:39:43 +0200

    Added Sidebar Settings support

--- lib/backend/tpl/subpage_sidebar.php
+++ lib/backend/tpl/subpage_sidebar.php
@@ -34,5 +34,28 @@
 			echo $module->get_setting( 'bg_color_widget' )->run_type()->form();
 		?>
 	</div>
-
+    
+    <?php
+    // Sidebars - Alignment Settings
+    
+    if ( $count = count( $module->get_module( 'sv_sidebar' )->get_sidebars( $module ) ) > 0 ) {
+        $i = 0;
+        
+        echo '<h3 class="divider">' . __( 'Sidebar Alignments', 'sv100' ) . '</h3>';
+        
+		foreach ( $module->get_module( 'sv_sidebar' )->get_sidebars( $module ) as $sidebar ) {
+		    echo $i === 0 ? '<div class="sv_setting_flex">' : '';
+		    
+			echo $module->get_setting( $sidebar['id'] )->run_type()->form();
+			
+			$i++;
+			
+			if ( $i === 3 || $i === $count ) {
+				$i = 0;
+				
+			    echo '</div>';
+            }
+		}
+    }
+	?>
 </div>
