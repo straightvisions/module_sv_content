446d5623d6bc35cf70d2d780ed25961a5e8f8035
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-07-31 12:09:25 +0200

    change vnum

--- sv_content.php
+++ sv_content.php
@@ -2,7 +2,7 @@
 	namespace sv100;
 	
 	/**
-	 * @version         4.263
+	 * @version         4.268
 	 * @author			straightvisions GmbH
 	 * @package			sv100
 	 * @copyright		2019 straightvisions GmbH
