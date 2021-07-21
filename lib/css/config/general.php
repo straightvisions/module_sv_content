687371607eaed8969761da2a3d4f1dd9b7b566ce
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-07-21 15:57:53 +0200

    REFACTORING FIX 404 Template Loading

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,13 +1,7 @@
 <?php
-	/* css namespace */
-	$namespace = 'sv100_sv_content';
-
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper:before' : '.sv100_sv_content_wrapper',
-		array_merge(
-			$module->get_setting('bg_color')->get_css_data('background-color'),
-			$module->get_setting('border')->get_css_data()
-		)
+		$module->get_setting('bg_color')->get_css_data('background-color')
 	);
 
 	echo $_s->build_css(
