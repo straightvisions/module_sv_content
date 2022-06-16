dbbece3f59a1b6d8b941e884ccf7d6e518b432ee
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-16 12:59:02 +0200

    fix default 404 layout

--- lib/tpl/frontend/single_404.php
+++ lib/tpl/frontend/single_404.php
@@ -3,13 +3,20 @@
 	?>
 	<div class="<?php echo $this->get_prefix(); ?>">
 		<div class="<?php echo $this->get_prefix( 'header' ); ?>">
-			<div class="<?php echo $this->get_prefix( 'header_content' ); ?>">
-				<h1><?php _e( 'Page not found', 'sv100' ); ?></h1>
-				<h3>
-					<a href="<?php echo home_url(); ?>">
-						<?php _e( 'Bring me back to the homepage', 'sv100' ); ?>
-					</a>
-				</h3>
+			<div class="<?php echo $this->get_prefix( 'header_content' ); ?>"></div>
+			<div class="<?php echo $this->get_prefix( 'wrapper' ); ?>">
+				<article>
+					<div class="wp-block-group">
+						<div class="wp-block-group__inner-container">
+							<h1><?php _e( 'Page not found', 'sv100' ); ?></h1>
+							<h3>
+								<a href="<?php echo home_url(); ?>">
+									<?php _e( 'Bring me back to the homepage', 'sv100' ); ?>
+								</a>
+							</h3>
+						</div>
+					</div>
+				</article>
 			</div>
 		</div>
 	</div>
